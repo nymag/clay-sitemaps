4be5cd3e7d8d022bac2ac206a877612632c5d25e
Chris Kirk <cperryk@gmail.com>
2017-02-21 16:32:03 -0500

    Refactors, adds README

---
+++ README.md
@@ -0,0 +1,79 @@
+# clay-sitemaps
+
+A suite a tools for generating sitemaps for Clay sites.
+
+Clay-sitemaps provides two "standard" sitemap middleware, one for text and one for XML, that can get
+you up and running quickly.
+
+It also provides utilities that can help you create your own sitemap logic.
+
+## Quick Start
+
+```
+const sitemaps = require('clay-sitemaps')(someAmphoraInstance);
+
+app
+  .get('/sitemap.txt', sitemaps.standardText())
+  .get('/sitemap.xml', sitemaps.standardXML({
+    engines: { // engines for rendering sitemap template
+      handlebars: yourHandlebarsEngine
+    }
+  }));
+```
+
+This will create a textual sitemap at `/sitemap.txt` that includes URLs of published,
+public pages.
+
+It will also create an XML sitemap at `/sitemap.xml` that includes published, public pages. Each
+page will appear as a `<url>` block with a `<loc>` set to the page's `url` property. Additionally,
+the `sitemap` template of each component on that page will be included in that block.
+
+## Advanced usage
+
+You can also use the functions behind clay-sitemaps to construct your own sitemap generation logic.
+
+### sitemaps.streamPages(prefix) ###
+
+Creates an object stream of ALL pages in amphora with `prefix`. These objects look like:
+
+```
+{
+  pageRef: string,
+  pageData: object
+}
+```
+
+The `pageData` value is the page data **not** composed, i.e. it includes top-level component
+references but no component data.
+
+### Filters
+
+Filters are functions that remove pages from the stream.
+
+#### sitemaps.filters.publicFilter()
+
+Returns a transform function that keeps only public pages from a pages stream.
+
+#### sitemaps.filters.publishedFilter()
+
+Returns a transform function that keeps only published pages from a pages stream.
+
+### Transforms
+
+Transforms are functions that modify a stream of pages or strings.
+
+#### sitemaps.transforms.pages.toText()
+
+Returns a transform function that converts a stream of pages to a stream of strings reflecting the URL of each page.
+
+#### sitemaps.transforms.pages.toXML(locals, multiplex)
+
+Returns a transform function that converts a stream of pages to a stream of XML including the URL of each page and rendered `sitemap` templates of all components on each page.
+
+#### sitemaps.transforms.pages.compose(locals)
+
+Returns a transform function that composes the `pageData` property in each item in a stream of pages so it includes all component data on that page.
+
+#### sitemaps.transforms.strings.addBookends(prelude, postlude)
+
+Returns a transform function that adds strings to the beginning and/or end of a stream of strings.
--- index.js
+++ index.js
@@ -3,55 +3,80 @@
 const _ = require('lodash'),
   multiplexTemplates = require('multiplex-templates'),
   handlebars = require('handlebars'),
-  streamPages = require('./page-streams').streamPages,
-  composePages = require('./page-streams').composePages,
-  filters = require('./filters'),
-  transforms = require('./transforms'),
+  pumpify = require('pumpify'),
+  streamPages = require('./lib/stream-pages'),
+  Filters = require('./lib/filters'),
+  Transforms = require('./lib/transforms'),
   DEFAULT_XML_PRELUDE = require('./constants').DEFAULT_XML_PRELUDE,
   DEFAULT_XML_POSTLUDE = require('./constants').DEFAULT_XML_POSTLUDE;
 
 /**
- * Returns an Express router for an article sitemap.
- * @param  {object} amphora - An instance of amphora
- * @param  {object} opts - Configuration options
- * @param  {string} opts.xmlPrelude - String to prepend to the XML
- * @param  {string} opts.xmlPostlude - String to append to the XML
- * @param  {function} opts.xmlTransform - Function that receives `locals` and returns a custom
- * stream transform object that converts each item in the stream into XML.
- * @param  {object} opts.engines - Multiplex engines object, for rendering sitemap template, if
- * no custom xmlTransform is defined.
- * @return {object} An express.Router()
+ * Generate a standard text sitemap middleware for Clay. The page URL of each page is included.
+ * Only published, public pages are included.
+ * @param  {object} amphora
+ * @return {string}
  */
-function generateSitemap(amphora, opts) {
+function standardText(amphora) {
+  const filters = Filters(amphora),
+    transforms = Transforms(amphora);
 
-  opts = _.defaults(opts, {
-    engines: {handlebars: handlebars},
-    xmlPrelude: DEFAULT_XML_PRELUDE,
-    xmlPostlude: DEFAULT_XML_POSTLUDE,
-    xmlTransform: (locals) => transforms.pages.toXML(locals, multiplexTemplates(opts.engines))
+  return (req, res) => {
+    res.type('text');
+    streamPages(amphora, res.locals.site.prefix)
+      .pipe(filters.published())
+      .pipe(filters.public())
+      .pipe(transforms.pages.toText(res.locals))
+      .pipe(res);
+  };
+}
+
+/**
+ * Generate a standard XML sitemap middleware for Clay. This includes only published, public pages.
+ * The "sitemap" templates of all the components on each page are rendered and included in
+ * the sitemap.
+ * @param  {object} amphora
+ * @param  {object} opts
+ * @param  {string} opts.prelude - XML to appear before any <url> object
+ * @param  {string} opts.postlude - XML to appear after last <url> object
+ * @param  {object} opts.engines - Multiplex engines configuration.
+ * @return {function}
+ */
+function standardXML(amphora, opts) {
+  const filters = Filters(amphora),
+    transforms = Transforms(amphora);
+
+  _.defaults(opts, {
+    prelude: DEFAULT_XML_PRELUDE,
+    postlude: DEFAULT_XML_POSTLUDE,
+    engines: handlebars
   });
 
+  return (req, res) => {
+    res.type('xml');
+    streamPages(amphora, res.locals.site.prefix)
+      .pipe(pumpify.obj([
+        filters.published(),
+        filters.public(),
+        transforms.pages.compose(res.locals),
+        transforms.pages.toXML(res.locals, multiplexTemplates(opts.engines)),
+        transforms.strings.addBookends(opts.prelude, opts.postlude)
+      ].filter(_.identity)))
+      .pipe(res);
+  };
+}
+
+/**
+ * Binds amphora to each method as necessary so devs don't
+ * have to keep passing it around on the outside.
+ * @param  {object} amphora
+ * @return {object} - This lib, with amphora bound to all methods that use it
+ */
+module.exports = function (amphora) {
   return {
-    txt: function (req, res) {
-      res.type('text');
-      streamPages(amphora, res.locals.site.prefix)
-        .pipe(filters.published())
-        .pipe(filters.public(amphora))
-        .pipe(transforms.pages.toText(res.locals))
-        .pipe(res);
-    },
-    xml: function (req, res) {
-      res.type('xml');
-      streamPages(amphora, res.locals.site.prefix)
-        .pipe(filters.published())
-        .pipe(filters.public(amphora))
-        .pipe(composePages(amphora, res.locals))
-        .pipe(opts.xmlTransform(res.locals))
-        .pipe(transforms.strings.addBookends(opts.xmlPrelude, opts.xmlPostlude))
-        .pipe(res);
-    }
+    standardText: standardText.bind(this, amphora),
+    standardXML: standardXML.bind(this, amphora),
+    streamPages: streamPages.bind(this, amphora),
+    filters: Filters(amphora),
+    transforms: Transforms(amphora)
   };
 };
-
-module.exports.filters = filters;
-module.exports.generateSitemap = generateSitemap;
