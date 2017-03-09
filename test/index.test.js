d71698ca3aa1de0bdfc39479b2c19f4aeadb3586
Chris Kirk <cperryk@gmail.com>
2017-03-09 18:01:24 -0500

    Adds eslint dev-dep, fixes expectValues to check length of results

--- test/index.test.js
+++ test/index.test.js
@@ -7,24 +7,44 @@ const lib = require('../index')(require('amphora')),
   components = require('amphora').components,
   multiplex = require('multiplex-templates')(require('handlebars')),
   composer = require('amphora').composer,
-  stream = require('stream'),
-  mockStream = (chunks, objectMode) => {
-    const s = new stream.Readable({objectMode: objectMode, encoding: objectMode ? null : 'utf8'});
+  stream = require('stream');
 
-    chunks.forEach(chunk => s.push(chunk));
-    s.push(null);
-    return s;
-  },
-  expectObjects = (values) => {
-    let i = 0;
+function mockStream(chunks, objectMode){
+  const s = new stream.Readable({objectMode: objectMode, encoding: objectMode ? null : 'utf8'});
 
-    return (val) => expect(val).to.deep.equal(values[i++]);
-  },
-  expectStrings = (values) => {
-    let i = 0;
+  chunks.forEach(chunk => s.push(chunk));
+  s.push(null);
+  return s;
+};
 
-    return (val) => expect(val).to.equal(values[i++]);
-  };
+/**
+ * Checks if each value in the stream matches the expected values.
+ * @param  {object} stream
+ * @param  {array} values
+ * @param  {function} done
+ */
+function expectValues(stream, values, done) {
+  let i = 0;
+
+  stream
+    .on('data', (val) => {
+      const compare = values[i++];
+
+      if (typeof compare === 'object') {
+        expect(val).to.deep.equal(compare);
+      } else {
+        expect(val).to.equal(compare);
+      }
+    })
+    .on('end', () => {
+      if (i === values.length) {
+        done();
+      } else {
+        done(new Error(`Expected ${values.length} results but got ${i}`));
+      }
+    })
+    .on('error', done);
+};
 
 describe('stream-pages', function () {
   let sandbox;
@@ -50,9 +70,10 @@ describe('stream-pages', function () {
 
 
   describe('streamPages', function () {
+
     it ('Streams and parses pages', function (done) {
-      lib.streamPages('/some/site/prefix')
-        .on('data', expectObjects([
+      const stream = lib.streamPages('/some/site/prefix'),
+        expected = [
           {
             pageRef: '/foo/bar',
             pageData: {url: 'bar'}
@@ -61,47 +82,39 @@ describe('stream-pages', function () {
             pageRef: '/baz/zar@published',
             pageData: {url: 'zar'}
           }
-        ]))
-        .on('end', ()=> done())
-        .on('error', done);
+        ];
+
+      expectValues(stream, expected, done);
     });
   });
 
   describe('filterPublished', function () {
     it ('Filters out unpublished pages', function (done) {
-      lib.streamPages('/some/site/prefix')
-        .pipe(lib.filters.published())
-        .on('data', expectObjects([
+      const stream = lib.streamPages('/some/site/prefix').pipe(lib.filters.published()),
+        expected = [
           {
             pageRef: '/baz/zar@published',
             pageData: {url: 'zar'}
           }
-        ]))
-        .on('end', ()=>done())
-        .on('error', done);
+        ];
+
+      expectValues(stream, expected, done);
     });
   });
 
   describe('transforms.pages.toText', function () {
     it ('Transforms each page to text', function (done) {
-      lib.streamPages('/some/site/prefix')
-        .pipe(lib.transforms.pages.toText())
-        .on('data', expectStrings([
-          'bar\n',
-          'zar\n'
-        ]))
-        .on('end', () => done())
-        .on('error', done);
+      const stream = lib.streamPages('/some/site/prefix')
+        .pipe(lib.transforms.pages.toText());
+
+      expectValues(stream, ['bar\n', 'zar\n'], done);
     });
   });
 
   describe('transforms.pages.compose', function () {
     it ('Composes each page in the stream, retaining page configuration data', function (done) {
-      composer.composePage.onCall(0).returns(Promise.resolve({a: 'b'}));
-      composer.composePage.onCall(1).returns(Promise.resolve({c: 'd'}));
-      lib.streamPages('/some/site/prefix')
-        .pipe(lib.transforms.pages.compose())
-        .on('data', expectObjects([
+      const stream = lib.streamPages('/some/site/prefix').pipe(lib.transforms.pages.compose()),
+        expected = [
           {
             pageRef: '/foo/bar',
             pageData: {
@@ -116,52 +129,52 @@ describe('stream-pages', function () {
               c: 'd'
             }
           }
-        ]))
-        .on('end', ()=>done())
-        .on('error', done);
+        ];
+
+      composer.composePage.onCall(0).returns(Promise.resolve({a: 'b'}));
+      composer.composePage.onCall(1).returns(Promise.resolve({c: 'd'}));
+      expectValues(stream, expected, done);
     });
   });
 
   describe('transforms.strings.addBookends', function () {
     it('Adds a string to the start of a stream', function (done) {
-      mockStream(['1','2','3'], false)
-        .pipe(lib.transforms.strings.addBookends('start'))
-        .setEncoding('utf8')
-        .on('data', expectStrings(['start','1','2','3']))
-        .on('end', () => done())
-        .on('error', done);
+      const stream = mockStream(['1','2','3'], false)
+          .pipe(lib.transforms.strings.addBookends('start'))
+          .setEncoding('utf8'),
+        expected = ['start','1','2','3'];
+
+      expectValues(stream, expected, done);
     });
     it('Adds a string to the end of a stream', function (done) {
-      mockStream(['1','2','3'], false)
-        .pipe(lib.transforms.strings.addBookends(null, 'end'))
-        .setEncoding('utf8')
-        .on('data', expectStrings(['1','2','3','end']))
-        .on('end', () => done())
-        .on('error', done);
+      const stream = mockStream(['1','2','3'], false)
+          .pipe(lib.transforms.strings.addBookends(null, 'end'))
+          .setEncoding('utf8'),
+        expected = ['1','2','3','end'];
+
+      expectValues(stream, expected, done);
     });
     it('Adds both a prelude and postlude if both are specified', function (done) {
-      mockStream(['1','2','3'], false)
-        .pipe(lib.transforms.strings.addBookends('start', 'end'))
-        .setEncoding('utf8')
-        .on('data', expectStrings(['start', '1','2','3','end']))
-        .on('end', () => done())
-        .on('error', done);
+      const stream = mockStream(['1','2','3'], false)
+          .pipe(lib.transforms.strings.addBookends('start', 'end'))
+          .setEncoding('utf8'),
+        expected = ['start', '1','2','3','end'];
+
+      expectValues(stream, expected, done);
     });
     it('Adds a prelude and postlude even if there are no chunks in the stream', function (done) {
-      mockStream([], false)
+      const stream = mockStream([], false)
       .pipe(lib.transforms.strings.addBookends('start', 'end'))
-      .setEncoding('utf8')
-      .on('data', expectStrings(['start', 'end']))
-      .on('end', () => done())
-      .on('error', done);
+      .setEncoding('utf8');
+
+      expectValues(stream, ['start','end'], done);
     });
   });
 
   describe('transforms.pages.toXML', function () {
 
     it('Transforms a page to XML, rendering its component sitemap templates', function (done) {
-      const stream = mockStream([
-        {
+      const stream = mockStream([{
           pageData: {
             head: [
               {
@@ -175,19 +188,15 @@ describe('stream-pages', function () {
             url: 'http://a/url.html'
           },
           pageRef: '/foo/bar'
-        }
-      ], true);
+        }], true).pipe(lib.transforms.pages.toXML()),
+        expected = [
+          '<url><loc>http://a/url.html</loc><foo>bar</foo></url>'
+        ];
 
       components.getTemplate.withArgs('/components/a/i', 'sitemap').returns('sitemap.handlebars');
       multiplex.render.returns('<foo>bar</foo>');
 
-      stream
-        .pipe(lib.transforms.pages.toXML({}, multiplex))
-        .on('data', expectStrings([
-          '<url><loc>http://a/url.html</loc><foo>bar</foo></url>'
-        ]))
-        .on('end', () => done())
-        .on('error', done);
+      expectValues(stream, expected, done);
     });
 
     it('Throws an error if it receives a page with no URL', function (done) {
